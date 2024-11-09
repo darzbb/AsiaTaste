a560afc0d6e3407ef0ff4270534bf739de8e671e
darzbb <dariaberozkina.zip@gmail.com>
2024-11-09 12:34:48 +0200

    First

--- script-adapt-recipes.js
+++ script-adapt-recipes.js
@@ -54,7 +54,7 @@ window.addEventListener("resize", function () {
     if (window.innerWidth < 768) {
         // Зміна стилю для маленьких екранів
         document.querySelectorAll(".recipe").forEach(function (element) {
-            element.style.width = "32.75%";
+            element.style.width = "32%";
         });
         document.querySelectorAll(".recipe").forEach(function (element) {
             element.style.marginLeft = "10px";
